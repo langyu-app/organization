29f8854247259e865840ba97e932c995c6a77b19
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2022-07-25 18:16:27 -0700

    ➕ Switch to `elm-community/typed-svg`

--- Logos/Lang.elm
+++ Logos/Lang.elm
@@ -7,9 +7,10 @@ use paired with the Yu (Fish) Logo wherever two images are appropriate.
 
 -}
 
+import Color
 import Html exposing (Html)
-import Svg exposing (circle, g, polygon, svg)
-import Svg.Attributes
+import TypedSvg exposing (circle, g, polygon, svg)
+import TypedSvg.Attributes
     exposing
         ( cx
         , cy
@@ -22,6 +23,7 @@ import Svg.Attributes
         , version
         , viewBox
         )
+import TypedSvg.Types exposing (Paint(..), Transform(..), num)
 
 
 {-| The langyu wolf logo, a multi-colored tangram depicting a wolf. Intended for
@@ -29,58 +31,58 @@ use paired with the Yu (Fish) Logo wherever two images are appropriate.
 -}
 asSvg : Html Never
 asSvg =
-    svg [ viewBox "0 0 2560 2560", version "1.1" ]
-        [ circle [ r "1280", cy "1280", cx "1280", fill "#e2e9ee" ] []
+    svg [ viewBox 0 0 2560 2560, version "1.1" ]
+        [ circle [ r <| num 1280, cy <| num 1280, cx <| num 1280, fill <| Paint <| Color.rgb 0.886 0.914 0.933 ] []
         , circle
-            [ r "1173.2"
-            , cy "1280"
-            , cx "1280"
-            , fill "none"
-            , stroke "#043962"
-            , strokeWidth "106.8"
+            [ r <| num 1173.2
+            , cy <| num 1280
+            , cx <| num 1280
+            , fill PaintNone
+            , stroke <| Paint <| Color.rgb 0.016 0.224 0.384
+            , strokeWidth <| num 106.8
             ]
             []
-        , g [ transform "matrix(0.2669568,0,0,0.2669568,1467.9818,660.09407)" ]
+        , g [ transform [ Matrix 0.2669568 0 0 0.2669568 1467.9818 660.09407 ] ]
             [ polygon
-                [ transform "rotate(-120,676.6752,2682.8368)"
-                , fill "#60b5cc"
-                , points "2000,2000 2000,0 0,0"
+                [ transform [ Rotate -120 676.6752 2682.8368 ]
+                , fill <| Paint <| Color.rgb 0.376 0.71 0.8
+                , points [ ( 2000, 2000 ), ( 2000, 0 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(-120,609.25447,1985.0771)"
-                , fill "#5a6378"
-                , points "2000,2000 0,2000 0,0"
+                [ transform [ Rotate -120 609.25447 1985.0771 ]
+                , fill <| Paint <| Color.rgb 0.353 0.388 0.471
+                , points [ ( 2000, 2000 ), ( 0, 2000 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(60,433.01269,749.99998)"
-                , fill "#7fd13b"
-                , points "1000,0 1000,1000 0,1000 0,0"
+                [ transform [ Rotate 60 433.01269 749.99998 ]
+                , fill <| Paint <| Color.rgb 0.498 0.82 0.231
+                , points [ ( 1000, 0 ), ( 1000, 1000 ), ( 0, 1000 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(60,1472.2379,1349.9909)"
-                , fill "#f0ad00"
-                , points "1000,1000 0,1000 0,0"
+                [ transform [ Rotate 60 1472.2379 1349.9909 ]
+                , fill <| Paint <| Color.rgb 0.941 0.678 0
+                , points [ ( 1000, 1000 ), ( 0, 1000 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(-120,1244.3345,885.08004)"
-                , fill "#f0ad00"
-                , points "1000,1000 1000,0 0,0"
+                [ transform [ Rotate -120 1244.3345 885.08004 ]
+                , fill <| Paint <| Color.rgb 0.941 0.678 0
+                , points [ ( 1000, 1000 ), ( 1000, 0 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(60,-1806.1945,2228.4592)"
-                , fill "#60b5cc"
-                , points "0,1414.2 0,0 1414.2,1414.2"
+                [ transform [ Rotate 60 -1806.1945 2228.4592 ]
+                , fill <| Paint <| Color.rgb 0.376 0.71 0.8
+                , points [ ( 0, 1414.2 ), ( 0, 0 ), ( 1414.2, 1414.2 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(105,-2383.4337,537.01294)"
-                , fill "#7fd13b"
-                , points "1414.2,0 2121.32,707.1 707.1,707.1 0,0"
+                [ transform [ Rotate 105 -2383.4337 537.01294 ]
+                , fill <| Paint <| Color.rgb 0.498 0.82 0.231
+                , points [ ( 1414.2, 0 ), ( 2121.32, 707.1 ), ( 707.1, 707.1 ), ( 0, 0 ) ]
                 ]
                 []
             ]
--- Logos/Yu.elm
+++ Logos/Yu.elm
@@ -7,9 +7,10 @@ use paired with the Lang (Wolf) Logo wherever two images are appropriate.
 
 -}
 
+import Color
 import Html exposing (Html)
-import Svg exposing (circle, g, polygon, svg)
-import Svg.Attributes
+import TypedSvg exposing (circle, g, polygon, svg)
+import TypedSvg.Attributes
     exposing
         ( cx
         , cy
@@ -22,6 +23,7 @@ import Svg.Attributes
         , version
         , viewBox
         )
+import TypedSvg.Types exposing (Paint(..), Transform(..), num)
 
 
 {-| The langyu fish logo, a multi-colored tangram depicting a fish. Intended for
@@ -29,57 +31,57 @@ use paired with the Lang (Wolf) Logo wherever two images are appropriate.
 -}
 asSvg : Html Never
 asSvg =
-    svg [ viewBox "0 0 2560 2560", version "1.1" ]
-        [ circle [ r "1280", cy "1280", cx "1280", fill "#e2e9ee" ] []
+    svg [ viewBox 0 0 2560 2560, version "1.1" ]
+        [ circle [ r <| num 1280, cy <| num 1280, cx <| num 1280, fill <| Paint <| Color.rgb 0.886 0.914 0.933 ] []
         , circle
-            [ r "1173.2"
-            , cy "1280"
-            , cx "1280"
-            , fill "none"
-            , stroke "#043962"
-            , strokeWidth "106.8"
+            [ r <| num 1173.2
+            , cy <| num 1280
+            , cx <| num 1280
+            , fill PaintNone
+            , stroke <| Paint <| Color.rgb 0.016 0.224 0.384
+            , strokeWidth <| num 106.8
             ]
             []
-        , g [ transform "matrix(0.34090909,0,0,0.34090909,1401.5818,564.09091)" ]
+        , g [ transform [ Matrix 0.34090909 0 0 0.34090909 1401.5818 564.09091 ] ]
             [ polygon
-                [ transform "rotate(-90,2100,2100)"
-                , fill "#60b5cc"
-                , points "2000,2000 2000,0 0,0 "
+                [ transform [ Rotate -90 2100 2100 ]
+                , fill <| Paint <| Color.rgb 0.376 0.71 0.8
+                , points [ ( 2000, 2000 ), ( 2000, 0 ), ( 0, 0 ) ]
                 ]
                 []
             , polygon
-                [ fill "#5a6378"
-                , points "0,0 2000,2000 0,2000 "
+                [ fill <| Paint <| Color.rgb 0.353 0.388 0.471
+                , points [ ( 0, 0 ), ( 2000, 2000 ), ( 0, 2000 ) ]
                 ]
                 []
             , polygon
-                [ transform "translate(-1200,1600)"
-                , fill "#7fd13b"
-                , points "0,1000 0,0 1000,0 1000,1000 "
+                [ transform [ Translate -1200 1600 ]
+                , fill <| Paint <| Color.rgb 0.498 0.82 0.231
+                , points [ ( 0, 1000 ), ( 0, 0 ), ( 1000, 0 ), ( 1000, 1000 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(-90,100,1300)"
-                , fill "#f0ad00"
-                , points "0,0 1000,1000 0,1000 "
+                [ transform [ Rotate -90 100 1300 ]
+                , fill <| Paint <| Color.rgb 0.941 0.678 0
+                , points [ ( 0, 0 ), ( 1000, 1000 ), ( 0, 1000 ) ]
                 ]
                 []
             , polygon
-                [ transform "translate(-1200,2800)"
-                , fill "#f0ad00"
-                , points "0,0 1000,1000 1000,0 "
+                [ transform [ Translate -1200 2800 ]
+                , fill <| Paint <| Color.rgb 0.941 0.678 0
+                , points [ ( 0, 0 ), ( 1000, 1000 ), ( 1000, 0 ) ]
                 ]
                 []
             , polygon
-                [ transform "rotate(-135,-690.80123,1726.3425)"
-                , fill "#60b5cc"
-                , points "0,1414.2 0,0 1414.2,1414.2 "
+                [ transform [ Rotate -135 -690.80123 1726.3425 ]
+                , fill <| Paint <| Color.rgb 0.376 0.71 0.8
+                , points [ ( 0, 1414.2 ), ( 0, 0 ), ( 1414.2, 1414.2 ) ]
                 ]
                 []
             , polygon
-                [ transform "matrix(0.70710678,-0.70710678,-0.70710678,-0.70710678,-2400,3741.395)"
-                , fill "#7fd13b"
-                , points "707.1,707.1 0,0 1414.2,0 2121.32,707.1 "
+                [ transform [ Matrix 0.70710678 -0.70710678 -0.70710678 -0.70710678 -2400 3741.395 ]
+                , fill <| Paint <| Color.rgb 0.498 0.82 0.231
+                , points [ ( 707.1, 707.1 ), ( 0, 0 ), ( 1414.2, 0 ), ( 2121.32, 707.1 ) ]
                 ]
                 []
             ]
